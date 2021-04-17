c0eaf53be4a2c70e46b1251bc7b932f8cf34a39b
Cyrus <80824990+Cyrus-A-Lacaba@users.noreply.github.com>
2021-04-17 16:00:36 +0800

    Add files via upload

--- JS/resume.js
+++ JS/resume.js
@@ -1,5 +1,11 @@
+var i = 0;
+var x = 0;
+
+var txtm = [" "," ", " ","Software Developer", "Web Developer", "Android Programmer"];
+
 window.onload = function() {
   $("a.float").addClass("home-visibility")
+  var myfunc = setInterval(typeWriterEffect,100);
 };
 
 $(window).scroll(function() {
@@ -9,3 +15,27 @@ $(window).scroll(function() {
     $("a.float").fadeOut();
   }
 });
+
+function typeWriterEffect(){
+  var txtSource;
+  if(x<txtm.length) {
+    if (i<txtm[x].length) {
+
+      if ($("#Occupation").html().length > 0) {
+        $("#Occupation").html($("#Occupation").html().substr(0,$("#Occupation").html().length-1));
+      }
+
+      txtSource = $("#Occupation").html() + txtm[x].charAt(i) + "I";
+      $("#Occupation").html(txtSource);
+
+      i++;
+    } else {
+      i = 0;
+      $("#Occupation").html("");
+      x++;
+    }
+  } else {
+    x = 0;
+    // clearInterval(myfunc);
+  }
+}
